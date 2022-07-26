594e86116e79fa52338e06d30ec50b2635b0d3ce
Eikcaj.1998 <jw220998@gmail.com>
2022-07-26 16:26:52 +0200

    header

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="app">
+  <TheHeader />
   </div>
 </template>
 
 <script>
-
+import TheHeader from "./components/TheHeader.vue";
 
 export default {
   name: 'App',
   components: {
+    TheHeader,
   }
 }
 </script>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <img src="../assets/img/spotify-logo.png" alt="spotify-logo">
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+@import "../assets/sass/variables.scss";
+    header{
+        background-color: $headerBgColor ;
+
+        img{
+          height: 5.5rem;
+          padding: 0.8rem;
+          margin-left: 1rem;
+        }
+    }
+
+</style>
